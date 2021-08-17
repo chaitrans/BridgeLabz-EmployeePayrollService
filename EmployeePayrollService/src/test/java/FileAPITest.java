def5d6392e54a175ad62753dca9449e6547eabce
chaitra845 <chaitrachytra@gmail.com>
2021-08-17 19:35:44 +0530

    Added watch service watch particular directory along with all files and Sub-directories

--- EmployeePayrollService/src/test/java/FileAPITest.java
+++ EmployeePayrollService/src/test/java/FileAPITest.java
@@ -1,3 +1,48 @@
+//import static org.junit.Assert.*;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.stream.IntStream;
+//import java.nio.file.Files;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class FileAPITest {
+//    public static final String HOME = System.getProperty("user.home");
+//    public static final String PLAY_WITH_FILES = "\\IdeaProjects\\EmployeePayrollService\\TempFiles";
+//
+//    @Test
+//    public void givenPathWhenCheckedThenConfirm() throws IOException {
+//
+//        Path homePath = Paths.get(HOME);
+//        Assert.assertTrue(Files.exists(homePath));
+//
+//        Path playPath = Paths.get(HOME + PLAY_WITH_FILES);
+//        if (Files.exists(playPath))
+//            FileUtils.deleteFiles(playPath.toFile());
+//        Assert.assertTrue(Files.notExists(playPath));
+//
+//        Files.createDirectory(playPath);
+//        Assert.assertTrue(Files.exists(playPath));
+//
+//        IntStream.range(1, 10).forEach(counter -> {
+//            Path tempFilePath = Paths.get(playPath + "/temp" + counter);
+//            Assert.assertTrue(Files.notExists(tempFilePath));
+//            try {
+//                Files.createFile(tempFilePath);
+//            } catch (IOException e) {
+//            }
+//            Assert.assertTrue(Files.exists(tempFilePath));
+//        });
+//
+//        Files.newDirectoryStream(playPath).forEach(System.out::println);
+//        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+//                .forEach(System.out::println);
+//    }
+//
+//}
+
 import static org.junit.Assert.*;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -36,6 +81,7 @@ public class FileAPITest {
             Assert.assertTrue(Files.exists(tempFilePath));
         });
 
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
                 .forEach(System.out::println);
