3501ee2e0a50fc5fe415c36f381b554a40921116
chaitra845 <chaitrachytra@gmail.com>
2021-08-17 21:29:08 +0530

    Added print operation to display employee payrolls from a file

--- EmployeePayrollService/src/test/java/EmployeePayrollServiceTest.java
+++ EmployeePayrollService/src/test/java/EmployeePayrollServiceTest.java
@@ -7,11 +7,13 @@ public class EmployeePayrollServiceTest {
     @Test
     public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
         EmployeePayrollData[] arrayOfEmployees = {
-                new EmployeePayrollData(1, "Chaitra NS", 800000.0),
-                new EmployeePayrollData(2, "Tushar Akhade", 850000.0),
-                new EmployeePayrollData(3, "Geeta Biradar", 900000.0) };
+                new EmployeePayrollData(1, "Chaitra NS", 8800000.0),
+                new EmployeePayrollData(2, "Geeta Biradar", 850000.0),
+                new EmployeePayrollData(3, "Ganga S", 900000.0) };
+
         EmployeePayrollService payrollServiceObject = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
         payrollServiceObject.writeEmployeeDdate(EmployeePayrollService.IOService.FILE_IO);
+        payrollServiceObject.printEmployeePayrollData();
 
         Assert.assertEquals(3, payrollServiceObject.countEnteries(EmployeePayrollService.IOService.FILE_IO));
     }
